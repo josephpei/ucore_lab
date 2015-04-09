be8b8f666bb14139e78bf624c04c4ee3002e14a0
Joseph Pei <josephpei@gmail.com>
2015-04-09 23:57:33 +0800

    LAB3

--- labcodes/lab3/kern/mm/default_pmm.c
+++ labcodes/lab3/kern/mm/default_pmm.c
@@ -72,12 +72,13 @@ default_init_memmap(struct Page *base, size_t n) {
     for (; p != base + n; p ++) {
         assert(PageReserved(p));
         p->flags = p->property = 0;
+        ClearPageProperty(p);
         set_page_ref(p, 0);
     }
-    base->property = n;
     SetPageProperty(base);
+    base->property = n;
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    list_add_before(&free_list, &(base->page_link));
 }
 
 static struct Page *
@@ -95,13 +96,15 @@ default_alloc_pages(size_t n) {
             break;
         }
     }
+
     if (page != NULL) {
-        list_del(&(page->page_link));
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+            SetPageProperty(p);
+            list_add(&(page->page_link), &(p->page_link));
+        }
+        list_del(&(page->page_link));
         nr_free -= n;
         ClearPageProperty(page);
     }
@@ -119,24 +122,34 @@ default_free_pages(struct Page *base, size_t n) {
     }
     base->property = n;
     SetPageProperty(base);
-    list_entry_t *le = list_next(&free_list);
-    while (le != &free_list) {
+    list_entry_t *le = &free_list;
+    p = NULL;
+    while((le = list_next(le)) != &free_list) {
         p = le2page(le, page_link);
-        le = list_next(le);
+        if (p > base)
+            break;
+    }
+    le = (p > base) ? (&(p->page_link)) : (&free_list);
+    list_add_before(le, &(base->page_link));
+    if (le != &free_list) {
         if (base + base->property == p) {
             base->property += p->property;
+            p->property = 0;
             ClearPageProperty(p);
             list_del(&(p->page_link));
         }
-        else if (p + p->property == base) {
+    }
+    le = list_prev(&(base->page_link));
+    if (le != &free_list) {
+        p = le2page(le, page_link);
+        if (p + p->property == base) {
             p->property += base->property;
+            base->property = 0;
             ClearPageProperty(base);
-            base = p;
-            list_del(&(p->page_link));
+            list_del(&(base->page_link));
         }
     }
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
 }
 
 static size_t
